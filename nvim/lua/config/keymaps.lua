ec439d617942047233785fd4b5d2bfa3334a01eb
Igor Cheliadinski <igorxciv@pm.me>
2026-07-06 07:22:13 -0700

    feat: center cursor after scrolling and search jumps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Center cursor after half-page scrolling
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down and center" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up and center" })
+
+-- Center cursor after full-page scrolling
+vim.keymap.set("n", "<C-f>", "<C-f>zz", { desc = "Scroll forward and center" })
+vim.keymap.set("n", "<C-b>", "<C-b>zz", { desc = "Scroll back and center" })
+
+-- Center cursor after jumping between search results
+vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result and center" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Prev search result and center" })
